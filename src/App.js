1eb4db073cbffc952b14ea5fded869de1736ebfe
ImaniGomez <imanimichelle123@gmail.com>
2025-05-16 20:53:47 -0400

    Initial commit of react code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
+import Navbar from './Navbar';
+import Contact from './Contact';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar />
+    <div className="main-content">
+        <h1 className = "greeting">Hi, There!</h1>
+        <p className="greeting-paragraph">Hello! My name is Imani Gomez. I am a computer engineering graduate <br></br>
+        student at NYU. My background in computer engineering and film <br></br>
+        production allows me to use my skills in machine learning, full stack<br></br> 
+        development, and digital media workflows to drive innovation in the<br></br> 
+        entertainment industry. This gives me a unique perspective on how <br></br>
+        technology shapes the future of storytelling and I am constantly<br></br> 
+        looking for new opportunities to learn and advance the industry forward.<br></br>
+        <br></br>
+        Additionally, I have a passion for the art form of photography. I have <br></br>
+        been shooting for 10 years and while I mostly focus on sports, I also<br></br> 
+        love anything from portraits to fashion to street photography. </p>
     </div>
+
+    <div className="work-content">
+        <span className="work-title">MY WORK</span>
+
+        <div className="work-block-pages">
+          <div className="work-block">Photography</div>
+          <div className="work-block">Software Projects</div>
+          <div className="work-block">Films</div>
+        </div>
+    </div>
+
+    <Contact />
+
+    </>
   );
 }
 
---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Contact.css';
+
+function Contact() {
+  return (
+    <div className="contact-content">
+      <span className="contact-title">Contact</span>
+
+      <div className="contact-bar">
+        <p><a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a></p>
+        <p>Email: abc@gmail.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
